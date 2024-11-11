d2a891e8df90049a512053bdbc36de7c02b4bbd8
yewon kim <yewonkim@yewonui-MacBookPro.local>
2024-11-11 20:44:27 +0900

    add. 인증 미들웨어 구현

--- src/constant/message.constant.js
+++ src/constant/message.constant.js
@@ -27,6 +27,13 @@ export const MESSAGES = {
           REQUIRED: '주소를 입력해 주세요.',
         },
         UNAUTORIZED: '인증 정보가 유효하지 않습니다.',
+        JWT: {
+          NO_TOKEN: '인증 정보가 없습니다.',
+          NOT_SUPPORTED_TYPE: '지원하지 않는 인증 방식입니다.',
+          EXPIRED: '인증 정보가 만료되었습니다.',
+          NO_USER: '인증 정보와 일치하는 유저가 없습니다.',
+          INVALID: '인증 정보가 유효하지 않습니다.',
+        },
       },
       SIGN_UP: {
         SUCCEED: '회원가입에 성공했습니다.',
